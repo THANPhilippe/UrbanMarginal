f52837bb0eee7191db59e39fe1baae538b2331b3
phili <phili@DESKTOP-QEGHPOI>
2016-09-13 19:25:50 +0200

    1er commit

---
+++ Urban Marginal/src/controleur/Controle.java
@@ -0,0 +1,40 @@
+package controleur;
+
+import javax.swing.JFrame;
+
+import outils.connexion.ClientSocket;
+import outils.connexion.ServeurSocket;
+import vue.EntreeJeu;
+
+public class Controle {
+	
+	private EntreeJeu frmEntreeJeu;
+	
+	public void evenementVue(JFrame uneFrame, Object info) {
+		if(uneFrame instanceof EntreeJeu){
+			evenementEntreeJeu(info);
+		}
+	}
+	
+	private void evenementEntreeJeu(Object info) {
+		if((String)info == "serveur"){
+			ServeurSocket serveursocket = new ServeurSocket(this, 6666);
+		}
+		else{
+			ClientSocket clientsocket;
+			(clientsocket = new ClientSocket ((String)info, 6666, this)).isConnexionOk();
+		}
+	}
+
+	public Controle () {
+		this.frmEntreeJeu = new EntreeJeu(this);
+		frmEntreeJeu.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		
+		new Controle();
+		
+	}
+
+}
---
+++ Urban Marginal/src/outils/connexion/Connection.java
@@ -0,0 +1,55 @@
+package outils.connexion;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
+
+public class Connection extends Thread {
+	
+	private Object leRecepteur;
+	private ObjectInputStream in;
+	private ObjectOutputStream out;
+	
+	public void run(){
+		boolean inOk = true;
+		Object reception;
+		while(inOk = true){
+			try {
+				reception = in.readObject();
+			} catch (ClassNotFoundException e) {
+				System.out.println("Classe Object non trouvée : "+e);
+				System.exit(0);
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, "l'ordinateur distant s'est déconnecté");
+				inOk = false;
+				try {
+					in.close();
+				} catch (IOException e1) {
+					System.out.println("erreur lors de la fermeture du canal : "+e1);
+				}
+			}
+		}
+	}
+	
+	public Connection(Socket socket, Object leRecepteur){
+		this.leRecepteur = leRecepteur;
+		try {
+			out = new ObjectOutputStream(socket.getOutputStream());
+		} catch (IOException e) {
+			System.out.println("erreur grave lors de la création du canal de sortie : "+e);
+			System.exit(0);
+		}
+		
+		try {
+			in = new ObjectInputStream(socket.getInputStream());
+		} catch (IOException e) {
+			System.out.println("erreur grave lors de la création du canal d'entrée : "+e);
+			System.exit(0);
+		}
+		
+		start();
+	}
+}
